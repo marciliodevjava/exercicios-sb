e64ac4121559426e3b030d8d2e91fb8752939c94
marciliodevjava <marciliodevjava@gmail.com>
2022-06-10 09:09:23 -0300

    Class PrimeiroControllers com RestFull

--- src/main/java/br/com/cod3r/exerciciossb/controllers/PrimeiroControllers.java
+++ src/main/java/br/com/cod3r/exerciciossb/controllers/PrimeiroControllers.java
@@ -1,13 +1,12 @@
 package br.com.cod3r.exerciciossb.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // rest controle
 public class PrimeiroControllers {
-	
-	@RequestMapping(method = RequestMethod.GET) // mapeando o metodo rest
+
+	@GetMapping(path = "/ola") // mapeando o metodo rest
 	public String ola() {
 		return "Olá Spring Boot";
 	}
